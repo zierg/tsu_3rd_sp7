388bd6bd29d83b219c9dd13d59699a34cd09f2c3
Sergey Guzhov <sergeyguzhov@gmail.com>
2014-03-30 23:37:00 +0400

    задание на третью неделю.

---
+++ src/main/java/w4/SinglyLinkedList.java
@@ -0,0 +1,30 @@
+package w4;
+
+/**
+ * Односвязный список целых чисел
+ */
+public class SinglyLinkedList {
+
+    /**
+     * Добавляет число i в конец списка
+     * @param i
+     */
+    public void push_back(Integer i) {
+
+    }
+
+    /**
+     * Изымает последний добавленный элемент списка
+     * @return
+     */
+    public Integer pop_back() {
+        throw new RuntimeException("list is empty!");
+    }
+
+    /**
+     * Сортирует односвязный список.
+     */
+    void sort() {
+
+    }
+}
---
+++ src/test/java/W2and3IntegerMultisetTest.java
@@ -0,0 +1,5 @@
+/**
+ * будет готово чуть позже...
+ */
+public class W2and3IntegerMultisetTest {
+}
