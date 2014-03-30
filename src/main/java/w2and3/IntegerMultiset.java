2b1fa05e595c865f8a242d741f1e90b783f23e46
Sergey Guzhov <sergeyguzhov@gmail.com>
2014-03-30 16:48:38 +0400

    набросал первое задание недели 2 и 3

---
+++ src/main/java/w2and3/IntegerMultiset.java
@@ -0,0 +1,53 @@
+package w2and3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Вы знаете, что такое "множество". В java для работы со множествами используют классы, реализующие
+ * интерфейс "Set" (например, HashSet и TreeSet).
+ * Вашей задачей будет реализовать мультимноджество, т.е. множество, содержащее одинаковые элементы.
+ * (а точнее, все методы этого класса). Для упрощения задачи мультимножество будет содержать только
+ * целые числа (Integer).
+ */
+public class IntegerMultiset {
+    public IntegerMultiset() {
+        Set<Integer> s = new HashSet<>();
+    }
+
+    /**
+     * Добавляет элемент "e"
+     * @param e
+     * @return true, если в мультимножестве уже есть такой элемент и false иначе
+     */
+    public boolean add(Integer e) {
+        return false;
+    }
+
+    /**
+     * Есть ли элемент "e"?
+     * @param e
+     * @return true, если элемент "e" присутствует в мультимножестве и false иначе
+     */
+    public boolean contains(Integer e) {
+        return false;
+    }
+
+    /**
+     * Удаляет один элемент "e"
+     * @param e
+     * @return true, если удаление имело место (т.е. "e" был в мультимножестве перед удалением) и false иначе
+     */
+    public boolean pop(Integer e) {
+        return false;
+    }
+
+    /**
+     * Удаляет все элементы, равные "e".
+     * @param e
+     * @return true, если удаление имело место и false иначе
+     */
+    public boolean remove(Integer e) {
+        return false;
+    }
+}
