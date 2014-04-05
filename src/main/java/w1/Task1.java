127e5e8062a32239ff698417df64be828a4c5451
Иван <zierg@yandex.ru>
2014-04-05 18:26:32 +0400

    w1 task1, количество уникальных элементов

--- src/main/java/w1/Task1.java
+++ src/main/java/w1/Task1.java
@@ -1,5 +1,7 @@
 package w1;
 
+import java.util.Arrays;
+
 
 /**
  * Примечание: в этой задаче не рекомендуется использовать контейнеры.
@@ -13,7 +15,21 @@ public class Task1 {
      * @return количество уникальных чисел
      */
     public int numberOfUniqueNumbers(int[] array) {
-        return array.length - 1;
+        if (array.length == 0) {
+            return 0;
+        }
+        int[] sortedArray = Arrays.copyOfRange(array, 0, array.length);
+        // Чтобы не изменять исходный массив
+        Arrays.sort(sortedArray);
+        int unique = 1;
+        int latestUnique = sortedArray[0];
+        for (int i = 1; i < sortedArray.length; i++) {
+            if (sortedArray[i] > latestUnique) {
+                latestUnique = sortedArray[i];
+                unique++;
+            }
+        }
+        return unique;
     }
 
     /**
